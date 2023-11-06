c7ebe963dc92771e10bab1c608314d63e7a7d5da
lzh <liuzh@gmail.com>
2023-11-06 19:04:45 +0800

    全新版本

--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -10,5 +10,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
+
+
     }
 }

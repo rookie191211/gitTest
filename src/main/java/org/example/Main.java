e53e462751905d6cd81c1db5da564eaa399e244f
lzh <liuzh@gmail.com>
2023-11-06 11:43:50 +0800

    新版本

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Created on ${DATE}.
+ *
+ * @author liu
+ * @content ${content}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/hello.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Created on 2023/11/6.
+ *
+ * @author liu
+ * @content
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

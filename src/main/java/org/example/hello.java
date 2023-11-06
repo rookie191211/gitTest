ce441b0a25b5fc7ac6d5ec494b1d2c68e9de2940
lzh <liuzh@gmail.com>
2023-11-06 11:48:10 +0800

    全新版本

--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -9,5 +9,6 @@ package org.example;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
